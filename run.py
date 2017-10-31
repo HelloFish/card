097e8fe75f1ebb03567a6e5db9116b09190d22fb
liuyun16 <larry1695@163.com>
2017-10-31 21:16:30 +0100

    change run.py + change register href

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 #启动web服务器脚本
 from app import app
+import os
 
-app.run(debug = True)
+if __name__ == '__main__':
+    #服务器用
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
+    #本地调试用
+    app.run(debug = True)
